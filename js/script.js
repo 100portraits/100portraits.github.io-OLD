75987e5f83f9eb60422d149ef54fe392c9cbaad2
100portraits <sahirde@gmail.com>
2021-08-07 16:30:06 -0700

    responsive text, more stufffff

--- js/script.js
+++ js/script.js
@@ -3,3 +3,18 @@ $(window).on('load', function() { // makes sure the whole site is loaded
   $('#preloader').delay(350).fadeOut('slow'); // will fade out the white DIV that covers the website. 
   $('body').delay(350).css({'overflow':'visible'});
 })
+
+function infoPopup() {
+  document.getElementById("bottomText").style.display = "none";
+  document.getElementById("infoText").style.display = "block";
+  document.getElementById("titlemid").innerHTML = "100portraits";
+  document.getElementById("titlemid").style.fontSize = "11.5vw";
+}
+
+function infoBack() {
+  document.getElementById("bottomText").style.display = "block";
+  document.getElementById("infoText").style.display = "none";
+  document.getElementById("titlemid").innerHTML = "Vancouver <br>Intl.";
+  document.getElementById("titlemid").style.fontSize = "17.0vw";
+
+}

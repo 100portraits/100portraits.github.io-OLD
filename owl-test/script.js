35686a16e22cbf753f0b4946e86f3fa68c065786
100portraits <sahirde@gmail.com>
2022-10-06 13:18:33 +0200

    add footer, reformat

--- owl-test/script.js
+++ owl-test/script.js
@@ -1,3 +1,22 @@
+$(window).on('load', function() { // makes sure the whole site is loaded 
+  $('#status').fadeOut(); // will first fade out the loading animation 
+  $('#preloader').delay(350).fadeOut('slow'); // will fade out the white DIV that covers the website. 
+  $('body').delay(350).css({'overflow':'visible'});
+})
+
+function infoPopup() {
+  document.getElementById("bottomText").style.display = "none";
+  document.getElementById("infoText").style.display = "block";
+
+}
+
+function infoBack() {
+  document.getElementById("bottomText").style.display = "block";
+  document.getElementById("infoText").style.display = "none";
+
+
+}
+
 	$(document).ready(function(){
 		$(".owl-carousel").owlCarousel({
 			center: true,

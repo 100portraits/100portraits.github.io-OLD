07026a6b173afa3ce9e13ea12844ae68f795945a
100portraits <sahirde@gmail.com>
2022-10-20 13:42:03 +0200

    mobile dark themed

--- mobile/script.js
+++ mobile/script.js
@@ -3,3 +3,7 @@
   $('#preloader').delay(350).fadeOut('slow'); // will fade out the white DIV that covers the website. 
   $('body').delay(350).css({'overflow':'visible'});
 })*/
+
+function scrollup() {
+  window.scrollTo({top: 0, behavior: 'smooth'});
+}
